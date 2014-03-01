c739e50a95065d9562aac019f829d8cb3bcf3514
Adam Gerstein <adam@gerstein.net>
2014-03-01 13:48:21 -0500

    Purge Users
    Allows you to remove existing accounts, using a perl script and ARD

---
+++ README.txt
@@ -0,0 +1,9 @@
+How to: remove all existing user accounts
+
+Using Apple Remote Desktop
+Select a computer (or group) and choose the "Manage" menu and then "Send UNIX Command..."
+
+The command to send:
+./Users/acc/purge_users.pl randomu1
+
+For "Run command as:" choose "User" and enter "root" as the user name. Make sure you choose to "Display all output".
---
+++ purge_users.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+###################################################################
+# usage:
+#	./Users/acc/purge_users.pl randomu1
+# when calling the script, you have to feed it a username to ignore - it could
+# be the last user in, or it can be anything you want (i.e. "randomu1")
+
+`cd /Users`;	#moe into the /Users directory
+#$homes=`ls /Users | grep -v -x "Shared" | grep -v -x "flamacadmin" | grep -v -x "acc" | grep -v -x @ARGV[0]`;
+# list the folders found in $root that aren't Shared, flamacadmin, acc, and put them into an array (@homes)
+
+#$homes=`find /Users -maxdepth 1 -newermt "2012-01-01 00:00" | grep -v -x "Shared" | grep -v -x "flamacadmin" | grep -v -x "acc"`; # | grep -v -x @ARGV[0]
+$homes=`find /Users -maxdepth 1 -mtime +7 | grep -v -x "Shared" | grep -v -x "flamacadmin" | grep -v -x "acc"`; # | grep -v -x @ARGV[0]
+# returns like: /Users/yankowskia2 
+
+# to export the list of users that's deleted?
+#open (EXPORT,'>>/Users/acc/export.txt');# or die 'can\'t open file';
+#$convertstring = join ("\n",@homes);
+#print EXPORT $convertstring;
+#close (EXPORT);
+
+@homes=split('\n', $homes);		# take the array, remove the \n and put them back in
+for($x=0;$x<=$#homes;$x++)		# for each entry in the array, do the following, then increment the value and start with the next entry
+{
+#	@homes[$x]="/Users/".@homes[$x];
+	# update the list from "username" to "/$root/username/$prefs"
+}
+
+foreach $i (@homes)		# for each entry in @homes
+{
+	print("$i\n");		# print out the user directory
+#	`rm -rf $i`;		# remove the users directory
+}
